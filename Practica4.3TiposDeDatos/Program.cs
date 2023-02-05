f077a57cd28665dab271fc789ee02b87ea46630c
Xabierr78 <116910820+Xabierr78@users.noreply.github.com>
2023-02-05 12:19:55 +0100

    ejercicio 4.3 Operaciones Basicas

---
+++ Practica4.3TiposDeDatos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.WriteLine("Escribe el primer numero por favor:");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Escribe el segundo numero por favor:");
+int b = int.Parse(Console.ReadLine());
+
+int suma = a + b;
+int resta = a - b;
+int multiplicacion = a * b;
+int division = a / b; 
+int residuo = a% b;
+
+
+
+Console.WriteLine("PRIMER NUMERO: " + a + System.Environment.NewLine + "SEGUNDO NUMERO: " + b + System.Environment.NewLine + "La suma es: " + suma + System.Environment.NewLine + "La resta es: " + resta + System.Environment.NewLine + "La multiplicacion es: " + multiplicacion
+    + System.Environment.NewLine + "La division es: " + division + System.Environment.NewLine + "La division es: " + division + System.Environment.NewLine + "El residuo es: " + residuo); 
+
+
